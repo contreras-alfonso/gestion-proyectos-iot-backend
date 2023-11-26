7ef656c960685338e0414c9eaf647aba3a1e1632
Alfonso Contreras <contrerasalfonso.lino@gmail.com>
2023-11-25 22:07:24 -0500

    jwt

--- controllers/notificacionController.js
+++ controllers/notificacionController.js
@@ -2,7 +2,7 @@ import Notificacion from "../models/notificacionModel.js"
 
 const getAll = async (req,res) => {
     const notificaciones = await Notificacion.find().sort({ _id: -1 }).limit(10).populate('dispositivo');
-    res.json(notificaciones.reverse());
+    res.json(notificaciones);
 }
 
 export{
